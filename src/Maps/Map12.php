373df341521cd6067de80e14c91e4cb0cfc5400f
Maxime <maxime.lecourt457@gmail.com>
2024-09-27 13:51:50 +0200

    Add map 1 to 12

---
+++ src/Maps/Map12.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace MudMgmk\Mud\Maps;
+
+use Jugid\Staurie\Component\Map\Blueprint;
+use Jugid\Staurie\Game\Position\Position;
+use MudMgmk\Mud\Items\Demon_Sword;
+use MudMgmk\Mud\Npcs\Garen;
+use MudMgmk\Mud\Npcs\Ornn;
+use MudMgmk\Mud\Monsters\Nashor;
+use MudMgmk\Mud\Monsters\Scuttle;
+
+class Map12 extends Blueprint
+{
+    private Position $position;
+
+    public function __construct()
+    {
+        $this->position = new Position(5, 4);
+    }
+
+    public function name(): string
+    {
+        return 'Path11';
+    }
+
+    public function description(): string
+    {
+        return 'This is a test map. This is a description where you can set a context element for users';
+    }
+
+    public function position(): Position
+    {
+        return $this->position;
+    }
+
+    public function npcs(): array
+    {
+        return [new Ornn(), new Garen()];
+    }
+    public function items(): array
+    {
+        return [new Demon_Sword()];
+    }
+
+    public function monsters(): array
+    {
+        return [new Scuttle()];
+    }
+}
