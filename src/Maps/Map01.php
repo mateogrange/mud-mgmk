58107508df3df3553261756345cf7df503182d96
Matéo Grange <grange.mateo45@gmail.com>
2024-09-27 10:27:44 +0200

    add Ornn and Garen npc

--- src/Maps/Map01.php
+++ src/Maps/Map01.php
@@ -5,37 +5,45 @@ namespace MudMgmk\Mud\Maps;
 use Jugid\Staurie\Component\Map\Blueprint;
 use Jugid\Staurie\Game\Position\Position;
 use MudMgmk\Mud\Items\Demon_Sword;
+use MudMgmk\Mud\Npcs\Garen;
+use MudMgmk\Mud\Npcs\Ornn;
 
-class Map01 extends Blueprint {
+class Map01 extends Blueprint
+{
 
-  private Position $position;
+    private Position $position;
 
-  public function __construct()
-  {
-      $this->position = new Position(0,0);
-  }
+    public function __construct()
+    {
+        $this->position = new Position(0, 0);
+    }
 
-  public function name() : string {
-      return 'Test map';
-  }
+    public function name(): string
+    {
+        return 'Test map';
+    }
 
-  public function description() : string {
-      return 'This is a test map. This is a description where you can set a context element for users';
-  }
+    public function description(): string
+    {
+        return 'This is a test map. This is a description where you can set a context element for users';
+    }
 
-  public function position() : Position {
-      return $this->position;
-  }
+    public function position(): Position
+    {
+        return $this->position;
+    }
 
-  public function npcs() : array {
-      return [];
-  }
+    public function npcs(): array
+    {
+        return [new Ornn(), new Garen()];
+    }
+    public function items(): array
+    {
+        return [new Demon_Sword()];
+    }
 
-  public function items() : array {
-      return [new Demon_Sword()];
-  }
-
-  public function monsters() : array {
-      return [];
-  }
+    public function monsters(): array
+    {
+        return [];
+    }
 }
---
+++ src/Npcs/Ornn.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace  MudMgmk\Mud\Npcs;
+
+use Jugid\Staurie\Game\Npc;
+use MudMgmk\Mud\Items\Demon_Sword;
+
+class Ornn extends Npc
+{
+
+  public function name(): string
+  {
+    return 'Ornn';
+  }
+
+  public function description(): string
+  {
+    return 'An ancient blacksmith with a temper as strong as his forge, crafting only for those worthy.';
+  }
+
+  public function speak(): string|array
+  {
+    if ($this->playerHasItem('')) {
+      return ['Please, do not hurt me !', 'I\'m famous here, I can help you !'];
+    } else {
+      $this->giveItem(new Demon_Sword());
+      return ['Hmpf. Another adventurer...Right, then. If you\'re going to fight, you\'re going to need this.
+      (Ornn quickly forges a sword, holding it out to you).
+      Take this sword. Go and cut your teeth on three Scuttles in the river. It\'s a good start for someone at your level.
+      Don\'t dawdle. If you come back alive, we\'ll see about something else.'];
+    }
+  }
+}
